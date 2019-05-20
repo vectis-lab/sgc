017ac8ef443413c2383df9135c7c5e1d7aa0ac8c
Andre Hermanto <andre.hermanto93@gmail.com>
2019-05-20 16:09:55 +1000

    elasticUrl staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -13,7 +13,7 @@ export const environment = {
     beaconNetworkUrl: 'https://beacon-network.org/api',
     vsalUrl: 'https://sgc.garvan.org.au/ssvs/query',
     vsalUrl2: 'https://vsal.garvan.org.au/vsal/core/find',
-    elasticUrl: 'https://52.62.174.102/_elasticsearch',
+    elasticUrl: 'https://dr-sgc.kccg.garvan.org.au/_elasticsearch',
     durlUrl: 'https://wt-ec1ac815dce38c76c2e7662693b82189-0.run.webtask.io/durl-dev',
     sentryUrl: 'https://90b2013bdfef4fef9491990e6ad379c6@sentry.io/158605',
     clinicalUrl: 'http://localhost:3000',
