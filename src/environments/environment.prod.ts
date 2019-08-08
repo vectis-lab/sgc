55e1ae271dfabc06e2d60eeb75c344786c45061d
Andre Hermanto <andre.hermanto93@gmail.com>
2019-08-08 16:22:27 +1000

    Change VSAL sgc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
     auth0Domain: 'sgc.au.auth0.com',
     auth0Connection: 'Production',
     beaconNetworkUrl: 'https://beacon-network.org/api',
-    vsalUrl: 'https://sgc.garvan.org.au/ssvs/query',
+    vsalUrl: 'https://vsal.garvan.org.au/ssvs/mgrb/query',
     elasticUrl: 'https://dr-sgc.kccg.garvan.org.au/_elasticsearch',
     durlUrl: 'https://wt-ec1ac815dce38c76c2e7662693b82189-0.run.webtask.io/durl',
     sentryUrl: 'https://6d4633523e614c36a0ba2a92358ab4cc@sentry.io/1471908',
