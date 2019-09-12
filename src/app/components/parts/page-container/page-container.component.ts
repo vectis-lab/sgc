999a1c10d0908e71e74f1b1d56b84bd22316971b
Andre Hermanto <a.hermanto@garvan.org.au>
2019-09-12 20:09:56 +1000

    Add cohort samples info

--- src/app/components/parts/page-container/page-container.component.ts
+++ src/app/components/parts/page-container/page-container.component.ts
@@ -1,7 +1,8 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit, ChangeDetectorRef } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { ScrollService } from '../../../services/scroll-service';
 import { SearchBarService } from '../../../services/search-bar-service';
+import { COHORT_SAMPLES_INFO } from '../../../model/cohort-value-mapping'
 
 const MIN_NAV_WIDTH = 1285;
 
@@ -17,6 +18,10 @@ export class PageContainerComponent implements OnInit {
     title = '';
     smallTitle = '';
     showHamburger = false;
+    totalSamplesGen = '';
+    totalSamplesPhen = '';
+    cohort = ''
+
 
     @HostListener('window:resize') windowResized() {
         this.showHamburger = window.innerWidth <= MIN_NAV_WIDTH;
@@ -24,7 +29,8 @@ export class PageContainerComponent implements OnInit {
 
     constructor(private router: Router,
                 private scrollService: ScrollService,
-                public searchBarService: SearchBarService) {
+                public searchBarService: SearchBarService,
+                public cd: ChangeDetectorRef) {
         this.windowResized();
         this.router.events
             .filter((x, idx) => x instanceof NavigationEnd)
@@ -34,7 +40,20 @@ export class PageContainerComponent implements OnInit {
     }
 
     ngOnInit() {
-
+        this.searchBarService.selectedCohort.subscribe(cohort => {
+            this.cohort = cohort;
+            if(this.cohort === 'Demo'){
+                this.cohort = 'Demo from 1000 Genome Project';
+            }
+            if(COHORT_SAMPLES_INFO[cohort]){
+                this.totalSamplesGen = COHORT_SAMPLES_INFO[cohort]['gen'];
+                this.totalSamplesPhen = COHORT_SAMPLES_INFO[cohort]['phen'];
+            }else{
+                this.totalSamplesGen = '';
+                this.totalSamplesPhen = ''; 
+            }
+            this.cd.detectChanges();
+        })
     }
 
     updateScroll($event: any) {
