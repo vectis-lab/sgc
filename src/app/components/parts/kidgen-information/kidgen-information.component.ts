0fdb22250a703e397f306bf73dc1001bce9a4a61
Andre Hermanto <andre.hermanto93@gmail.com>
2019-12-05 19:06:30 +1100

    kidgen new charts

--- src/app/components/parts/kidgen-information/kidgen-information.component.ts
+++ src/app/components/parts/kidgen-information/kidgen-information.component.ts
@@ -28,13 +28,27 @@ export class KidgenInformationComponent {
 
     clinicalFields: ClinicalFields[] = [
         new ClinicalFields('sex', 'sex', 'Sex', 'pie'),
-        new ClinicalFields('A. Renal Impairment Please include test values, even is results are normal', 'renalImpairmanet', 'Renal Impairment', 'row'),
-        new ClinicalFields('i. Urea (mmol per L)', 'ureaMmolPerL', 'Urea (mmol/L)','row'),
-        new ClinicalFields('ii. Creatinine (umol per L)', 'creatinineUmolPerL', 'Creatinine (umol/L)', 'row'),
-        new ClinicalFields('iii. eGFR (mL per min per 1.73m_)', 'eGFRMlPerMin', 'eGFR (mL/min/1.73m)', 'row'),
-        new ClinicalFields('iv. CKD stage', 'CKDStage', 'CKD stage', 'pie'),
-        new ClinicalFields('v. Current RRT', 'currentRRT', 'Current RRT', 'pie'),
-        new ClinicalFields('B. Hypertension Enter results if available', 'hypertension', 'Hypertension', 'pie')
+        new ClinicalFields('i. Urea (mmol per L)', 'ureaMmolPerL', 'Urea (mmol/L)','bar', true, false, null, 340, 200, 0, 30),
+        new ClinicalFields('ii. Creatinine (umol per L)', 'creatinineUmolPerL', 'Creatinine (umol/L)', 'bar', true, false, null, 340, 200, 0, 200),
+        new ClinicalFields('iii. eGFR (mL per min per 1.73m_)', 'eGFRMlPerMin', 'eGFR (mL/min/1.73m)', 'bar', true, false, null, 340, 200, 0, 200),
+        new ClinicalFields('iv. CKD stage', 'CKDStage', 'CKD stage', 'row'),
+        new ClinicalFields('B. Hypertension Enter results if available', 'hypertension', 'Hypertension', 'pie'),
+        new ClinicalFields('Value Sodium', 'sodium', 'Value Sodium', 'bar', false, false, null, 340, 200, 120, 160),
+        new ClinicalFields('Value Potassium', 'potassium', 'Value Potassium', 'bar', false, false, null, 340, 200, 0, 10),
+        new ClinicalFields('Value Chloride', 'chloride', 'Value Chloride', 'bar', false, false, null, 340, 200, 90, 200),
+        new ClinicalFields('Value Bicarbonate', 'bicarbonate', 'Value Bicarbonate', 'bar', false, false, null, 340, 200, 15, 35),
+        new ClinicalFields('Value Calcium', 'calcium', 'Value Calcium', 'bar', false, false, null, 340, 200, 0, 5),
+        new ClinicalFields('Value Magnesium', 'magnesium', 'Value Magnesium', 'bar', false, false, null, 340, 200, 0, 5),
+        new ClinicalFields('Value Phosphate', 'phosphate', 'Value Phosphate', 'bar', false, false, null, 340, 200, 0, 5),
+        new ClinicalFields('Value Bilirubin', 'bilirubin', 'Value Bilirubin', 'bar', false, false, null, 340, 200, 0, 15),
+        new ClinicalFields('Value ALP', 'alp', 'Value ALP', 'bar', false, false, null, 340, 200, 0, 10),
+        new ClinicalFields('Value GGT', 'ggt', 'Value GGT', 'bar', false, false, null, 340, 200, 0, 70),
+        new ClinicalFields('Value ALT', 'alt', 'Value ALT', 'bar', false, false, null, 340, 200, 0, 80),
+        new ClinicalFields('Value AST', 'ast', 'Value AST', 'bar', false, false, null, 340, 200, 0, 40),
+        new ClinicalFields('Value Albumin', 'albumin', 'Value Albumin', 'bar', false, false, null, 340, 200, 0, 50),
+        new ClinicalFields('i. Haematuria', 'haematuria', 'i. Haematuria', 'pie'),
+        new ClinicalFields('ii. Proteinuria', 'proteinuria', 'ii. Proteinuria', 'pie'),
+        new ClinicalFields('Protein: Creatinine ratio value', 'protein', 'Protein: Creatinine ratio value', 'bar', false, false, null, 340, 200, 0, 1500),
     ];
     phenoService: string = 'getKidgen'
 
