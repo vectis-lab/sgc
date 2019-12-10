8e41bdbb4da8777a81849a64580df05f858e3396
Andre Hermanto <andre.hermanto93@gmail.com>
2019-12-10 17:28:05 +1100

    Change label for height and weight

--- src/app/services/clinapi.service.ts
+++ src/app/services/clinapi.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { FAKE_CLINICAL_DATA } from "../mocks/clindata";
-import { FAKE_DEMO_DATA, TEST_CIRCA_DATA } from "../mocks/demodata";
+import { FAKE_DEMO_DATA } from "../mocks/demodata";
 import { VariantSearchService } from './variant-search-service';
 import { Subscription } from 'rxjs/Subscription';
 import { of, throwError, Observable } from "rxjs";
@@ -9,7 +9,6 @@ import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import 'rxjs/add/operator/delay';
-import { TEST_MGRB } from "../data/mgrbdata";
 
 @Injectable()
 export class ClinapiService implements OnDestroy {
@@ -78,14 +77,13 @@ export class ClinapiService implements OnDestroy {
             headers: new HttpHeaders({'Authorization': `Bearer ${localStorage.getItem('idToken')}`})
         };
         if(authorize){
-            return of<any>(TEST_MGRB)
             return this.http.get<any>(`${environment.vsalUrl2}?pheno=true&dataset=mgrb`, httpOptions).map(res => {
                 return JSON.parse(res.pheno)
             });
         }//if not authorize but want to see demo
         else if(demo){
             console.log("DEMO")
-            return of<any>(TEST_CIRCA_DATA);
+            return of<any>(FAKE_DEMO_DATA);
         }//if not authorize and not opt to see demo
         else {
             return throwError({ status: 401 });
